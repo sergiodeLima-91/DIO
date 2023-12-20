625917e483e7141b2af9f4761f85796f26d65083
Sergio_Lima <sergiodelima22@gmail.com>
2023-12-20 11:33:54 -0300

    Erro de Nao Mostrar a Imagem e Detalhes da Carta no Mouse Over Corrigido

--- projetos-de-games/js-yugioh-assets/src/scripts/engine.js
+++ projetos-de-games/js-yugioh-assets/src/scripts/engine.js
@@ -3,25 +3,25 @@
 //Definindo o state, que guardará tudo o que será manipulado dentro do jogo.O STATE ele é um objeto que guarda outros objetos e seus estados.
 const state = {
     //Objeto SCORE
-    score:{
+    score: {
         playerScore: 0,
-        cumputerScore: 0,
+        computerScore: 0,
         scoreBox: document.getElementById("score_points")
     },
     cardSprites: {
         avatar: document.getElementById("card-image"),
         name: document.getElementById("card-name"),
-        type: document.getElementById("card-type"),
+        type: document.getElementById("card-type")
     },
     fieldCards: {
         player: document.getElementById("player-field-card"),
         computer: document.getElementById("computer-field-card")
     },
     playerSides: {
+        player1: "player-cards",
+        player1BOX: document.querySelector("#player-cards"),
         computer: "computer-cards",
-        player: "player-cards",
-        playerb: document.querySelector("#computer-cards"),
-        computerb: document.querySelector("#player-cards"),
+        computerBOX: document.querySelector("#computer-cards")
     },
     // No caso do botão não é necessário criar um objeto porque ele tem somente uma funcionalidade.
     actions: {
@@ -32,43 +32,38 @@ const state = {
 //Listando as cartas (enumerar) do jogo para recuperar elas depois:
 // O recomendado colocar os assests num banco de dados, mas aqui a proposta é simples.
 //Colocando o caminho das imagens numa constante para ser chamado por interpolação de strings:
-const pathImage = "./src/assets/icons/";
+const pathImages = "./src/assets/icons/";
 const cardData = [
     // Para cada registro deste vetor, vamos criar um objeto:
     {
         id: 0,
         name: "Blue Eyes White Dragon",
         type: "Paper",
-        img:  `${pathImage}dragon.png`,
-        //De que carta esta daqui perde ou ganha:
+        img: `${pathImages}dragon.png`,
         WinOf: [1],
         LoseOf: [2],
     },
-
     {
         id: 1,
         name: "Dark Magician",
         type: "Rock",
-        img:  `${pathImage}magician.png`,
-        //De que carta esta daqui perde ou ganha:
+        img: `${pathImages}magician.png`,
         WinOf: [2],
         LoseOf: [0],
     },
-
     {
         id: 2,
         name: "Exodia",
         type: "Scissors",
-        img:  `${pathImage}exodia.png`,
-        //De que carta esta daqui perde ou ganha:
+        img: `${pathImages}exodia.png`,
         WinOf: [0],
-        LoseOf: [1]
+        LoseOf: [1],
     }
 ];
 
 //Criando função que sorteia ID das cartas:
-async function getRandomCardId(){
-    const randomIndex = Math.floor(Math.random() * cardData.length)
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
     return cardData[randomIndex].id;
 }
 
@@ -87,7 +82,7 @@ async function createCardImage(IdCard, fieldSide){
     cardImage.classList.add("card");
 
     // Criando condcional para liberar seleção das cartas que são DO PLAYER e não do computador:
-    if(fieldSide === state.playerSides.player){
+    if(fieldSide === state.playerSides.player1){
         //Criando animação de apresentar a carta no painel esquerdao quando o mouse estiver encima dela:
         cardImage.addEventListener("mouseover", ()=> {
             drawSelectCard(IdCard);
@@ -105,10 +100,10 @@ async function setCardsField(cardId) {
     // Função que impede a seleção de quaisquer outras cartas quando já existem cartas nos fields
     await removeAllCardsImages();
     //Carta aleatória pro computador:
-    let computerCardId = getRandomCardId();
+    let computerCardId =  await getRandomCardId();
 
-    state.fieldCards.player.style.display = "block"
-    state.fieldCards.computer.style.display = "block"
+    state.fieldCards.player.style.display = "block";
+    state.fieldCards.computer.style.display = "block";
 
     //Setando as imagens:
     state.fieldCards.player.src = cardData[cardId].img;
@@ -117,17 +112,36 @@ async function setCardsField(cardId) {
     //Verificação de quem ganhou:
     let duelResults = await checkDuelResults(cardId, computerCardId);
 
-    //Função que atualiza a pontuação:
+    // //Função que atualiza a pontuação:
     await updateScore();
     await drawButton();
 };
+
+//Criando função que elimina as demais cartas quando uma é selecionada:
+async function removeAllCardsImages(){
+    //PARA O COMPUTADOR:
+    //Recuperar cards na memória por desetruturação:
+    let {computerBOX, player1BOX} = state.playerSides;
+    //Pegando elementos de imagem:
+    let imgElements = computerBOX.querySelectorAll("img");
+    // Removendo as imagens por laço de repetição:
+    imgElements.forEach((img) => img.remove())
+
+    //PARA O PLAYER:
+    //Recuperar cards na memória
+       cards = state.playerSides.player1BOX;
+       //Pegando elementos de imagem:
+       imgElements = player1BOX.querySelectorAll("img");
+       // Removendo as imagens por laço de repetição:
+       imgElements.forEach((img) => img.remove());
+}
 
 //Criando a função para desenhar a carta no painel esquerdo quando o mouse-pointer estiver encima
 async function drawSelectCard(index){
     state.cardSprites.avatar.src = cardData[index].img;
     // "innerText: Texto interno"
     state.cardSprites.name.innerText = cardData[index].name;
-    state.cardSprites.type.innerText = "Atributo :"  + cardData[index].type;
+    state.cardSprites.type.innerText = "Attribute :"  + cardData[index].type;
 };
 
 //Criando a função que sorteia as cartas:
@@ -146,7 +160,7 @@ async function drawCards(cardNumbers, fieldSide){
 // Função para chamar outras funções:
 function init(){
     //Sorteando as cartas para ambos os jogadores:
-    drawCards(5, state.playerSides.player);
+    drawCards(5, state.playerSides.player1);
     drawCards(5, state.playerSides.computer);
     }
 
