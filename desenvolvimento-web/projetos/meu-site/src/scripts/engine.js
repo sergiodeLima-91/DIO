7df5b0145dc870a1db6b522b226cb465016f9d0e
Sergio_Lima <sergiodelima22@gmail.com>
2024-01-02 08:36:31 -0300

    Criando novos estilos

--- desenvolvimento-web/projetos/meu-site/src/scripts/engine.js
+++ desenvolvimento-web/projetos/meu-site/src/scripts/engine.js
@@ -1,23 +1,28 @@
 function mostrarSaudacao() {
     var nome = "Sérgio";
     var mensagemSaudacao = document.getElementById("mensagemSaudacao");
-    mensagemSaudacao.innerHTML = "Olá, meu nome é " + nome + "! Prazer em conhecê-lo!<br><br> Nasci em Campina Grande, na Paraíba. Sou alguém que deseja sempre crescer intelectualmente em todos os ambitos da vida. Para poder manter a mente sempre ativa, procuro manter uma rotina de estudos diária tanto na área de tecnologia quanto em âmbito religioso por ser cristão evangélico. Desejo migrar de profissão saindo de humanas para a exatas por meio da tecnologia da informação no mundo da programação. Sou formado em Análise e Desenvolvimento de Sistemas pela Universidade UNOPAR e sigo a carreira em desenvolvimento de jogos."
-    mensagemSaudacao.classList.add("saudacao","fadeIn")
+    mensagemSaudacao.classList.add("saudacao","fadeIn");    
   }
 
 var timeoutId;
 
-  function exibirMensagemPequena() {
-    timeoutId = setTimeout(function(){
-    var balaoMensagem = document.getElementById("balaoMensagem");
-    balaoMensagem.style.display = "block";
-    }, 100);
-    
-  }
+function exibirMensagemPequena() {
+  timeoutId = setTimeout(function(){
+  var balaoMensagem = document.getElementById("balaoMensagem");
+  balaoMensagem.style.display = "block";
+  }, 100);
+  
+}
 
-  function ocultarMensagemPequena() {
-    clearTimeout(timeoutId);
-    var balaoMensagem = document.getElementById("balaoMensagem");
-    balaoMensagem.style.display = "none";
-  };
+function ocultarMensagemPequena() {
+  clearTimeout(timeoutId);
+  var balaoMensagem = document.getElementById("balaoMensagem");
+  balaoMensagem.style.display = "none";
+};
+
+var empurre = document.getElementById("imagem");
+
+empurre.addEventListener("click", function() {
+  this.classList.toggle("empurre")
+}); 
   
