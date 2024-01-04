b3106222aa342f8ab3813ecaaf9fc0f89bddcb6f
Sergio_Lima <sergiodelima22@gmail.com>
2024-01-04 10:36:46 -0300

    Encerramento - Debbug e Tratamento de Erros com JS

--- orbi-web-games-developer/Debugging-e-error-handling-em-js/atividade.js
+++ orbi-web-games-developer/Debugging-e-error-handling-em-js/atividade.js
@@ -1,20 +1,34 @@
 // Validação de Erros por Tipo:
 
-const meuArray = [5 ,3 ,4 ,6 ,9];
-const tamanhoArray = meuArray.length;
-const number = 4
-
-function validacaoDeArray(num) {
-    console.log(num)
-    console.log(tamanhoArray);
-    try {
-        if (num === meuArray.length) {
-            console.log('É igual!');
-        };
+function validaArray(array, numero) {
     
-    } catch{
-        console.log('Não é igual!');
-    };
-};
+    try {
+        if (!array && !numero) throw new ReferenceError('Digite os valores antes de chamar a função!')
 
-validacaoDeArray(number);
+        if (typeof array !== 'object') throw new TypeError("Arrray precisa ser do tipo objeto!")
+
+        if (typeof numero !== 'number') throw new TypeError("O número precisa ser do tipo number!")
+
+        if (array.length !== numero) throw new RangeError('Array e número com tamanhos diferentes!')
+        
+        // Caso oarray seja válido, ele será retornado:
+        return array
+
+    } catch (error) {
+        // Filtrando erros com o instanceof
+        // Se o parâmetro erro do catch é uma instância de ReferenceError...
+        if (error instanceof ReferenceError) {
+            console.log(`${error.name}: ${error.message}`);
+        } else if (error instanceof TypeError) {
+            console.log(`${error.name}: ${error.message}`);
+        } else if (error instanceof RangeError) {
+            console.log(`${error.name}: ${error.message}`)
+        } else {
+            console.log('Tipo de erro inesoerado: ' + error)
+        } 
+        
+    }
+
+}
+
+console.log(validaArray([3,2,4], 3));
