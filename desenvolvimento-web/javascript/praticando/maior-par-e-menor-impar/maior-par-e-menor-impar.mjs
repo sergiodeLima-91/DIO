9e3cf68f0b3729be02146dbff323daff3d4c6548
sergio-lima <sergiodelima22@gmail.com>
2024-04-18 09:23:08 -0300

    Primeiro commit

---
+++ desenvolvimento-web/javascript/praticando/maior-par-e-menor-impar/maior-par-e-menor-impar.mjs
@@ -0,0 +1,22 @@
+// Construa um algoritmo que imprima o MAIOR número par e o menor número ÍMPAR:
+
+const numeros = [1,2,3,4,5,6,-7,8,9,10];
+let maiorPar = 0;
+let menorImpar = 1;
+
+for (let i = 0; i < numeros.length; i++) {
+    // Maior Número Par:
+    if(numeros[i] %2 == 0) {
+        maiorPar = numeros[i];
+        if (numeros[i] > maiorPar) {
+            maiorPar = numeros[i];
+        }
+    } else {
+        if (numeros[i] < menorImpar) {
+            menorImpar = numeros[i];
+        }
+    }
+};
+console.log(`Maior PAR:${maiorPar}`);
+console.log(`Menor IMPAR:${menorImpar}`);
+
