37e0f5b419e94e553ce17251a339b9bf78527465
sergio-lima <sergiodelima22@gmail.com>
2024-04-18 14:14:38 -0300

    Refatoracao com funcoes

--- desenvolvimento-web/javascript/praticando/maior-par-e-menor-impar/maior-par-e-menor-impar.mjs
+++ desenvolvimento-web/javascript/praticando/maior-par-e-menor-impar/maior-par-e-menor-impar.mjs
@@ -1,26 +1,34 @@
-// Construa um algoritmo que imprima o MAIOR número par e o menor número ÍMPAR:
+// Construa um algoritmo que imprima o MAIOR número par e o menor número ÍMPAR de uma lista de números:
 
-const numeros = [1,2,3,4,5,6,-7,8,9,10];
-let maiorPar = 0;
-// Um sempre será o menor número impar da esquerda para a direita.
-let menorImpar = 1;
-
-for (let i = 0; i < numeros.length; i++) {
-    // Maior Número Par:
-    if(numeros[i] %2 == 0) {
-        maiorPar = numeros[i];
-        if (numeros[i] > maiorPar) {
-            maiorPar = numeros[i];
-        };
-    // Menor Número Impar:
-    } else {
-        if (numeros[i] < menorImpar) {
-            menorImpar = numeros[i];
+function maiorNumeroPar(num) {
+    let maiorPar = 0;
+    for (let i = 0; i < num.length; i++) {
+        if(num[i] %2 === 0) {
+            maiorPar = num[i];
+            if (num[i] > maiorPar) {
+                maiorPar = num[i];
+            }
         }
     }
+
+    return maiorPar;
 };
 
+function menorNumeroImpar(num) {
+    // Um sempre será o menor número impar da esquerda para a direita.
+    let menorImpar = 1;
+    for (let i = 0; i < num.length; i++) {
+        if (num %2 !== 0) {
+            if (num[i] < menorImpar) {
+                menorImpar = num[i];
+            }
+        }        
+    }
 
-console.log(`Maior PAR:${maiorPar}`);
-console.log(`Menor IMPAR:${menorImpar}`);
+    return menorImpar;
+};
 
+const numeros = [1,2,3,4,5,6,-7,8,9,10];
+
+console.log(`Maior PAR:${maiorNumeroPar(numeros)}`);
+console.log(`Menor IMPAR:${menorNumeroImpar(numeros)}`);
