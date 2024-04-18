7b8830536328a2cb173e2e35e49141f931a466af
sergio-lima <sergiodelima22@gmail.com>
2024-04-18 10:00:25 -0300

    Primeiro Commit

--- desenvolvimento-web/javascript/praticando/maior-par-e-menor-impar/maior-par-e-menor-impar.mjs
+++ desenvolvimento-web/javascript/praticando/maior-par-e-menor-impar/maior-par-e-menor-impar.mjs
@@ -2,6 +2,7 @@
 
 const numeros = [1,2,3,4,5,6,-7,8,9,10];
 let maiorPar = 0;
+// Um sempre será o menor número impar da esquerda para a direita.
 let menorImpar = 1;
 
 for (let i = 0; i < numeros.length; i++) {
@@ -10,13 +11,16 @@ for (let i = 0; i < numeros.length; i++) {
         maiorPar = numeros[i];
         if (numeros[i] > maiorPar) {
             maiorPar = numeros[i];
-        }
+        };
+    // Menor Número Impar:
     } else {
         if (numeros[i] < menorImpar) {
             menorImpar = numeros[i];
         }
     }
 };
+
+
 console.log(`Maior PAR:${maiorPar}`);
 console.log(`Menor IMPAR:${menorImpar}`);
 
